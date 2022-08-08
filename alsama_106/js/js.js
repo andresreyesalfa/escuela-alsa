19934450025bf2bac92c3dfb7a2bfa75ad10c407
andresreyesalfa <110208166+andresreyesalfa@users.noreply.github.com>
2022-08-08 10:21:56 -0300

    Add files via upload

---
+++ alsama_106/js/js.js
@@ -0,0 +1,9 @@
+/*<®> Inicialización <®>*/
+  $.getScript("js/md5.js");
+  $.getScript("js/mnus.js");
+  $.getScript("js/abms.js");
+  $.getScript("js/salir.js");
+  $.getScript("js/enviar.js");
+  $.getScript("js/confirma.js");
+  $.getScript("js/login.js");
+  
